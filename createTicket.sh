ba0316f94914e000a1f46e469938ef650eb0fbaf
zhcoder <zhcoder@yandex.ru>
2021-11-01 21:03:46 +1000

    add changelog and author

--- createTicket.sh
+++ createTicket.sh
@@ -1,6 +1,12 @@
 # !/bin/bash
 
 RELEASE_TAG=$(git describe --tags HEAD)
+EARLY_RELEASE=$(git tag -l | sort -r | tail -1 | head -n2)
+LAST_RELEASE=$(git tag -l | sort -r | head -n1)
+
+AUTHOR=$(git log --pretty=format:"%aN" | head -n1)
+CHANGELOG=$(git log ${EARLY_RELEASE}..${LAST_RELEASE} --pretty=format:"%h - %s (%an, %ar)")
+
 RESPONSE=$(
 curl -s -X POST https://api.tracker.yandex.net/v2/issues \
 --write-out '%{http_code}' \
@@ -12,7 +18,7 @@ curl -s -X POST https://api.tracker.yandex.net/v2/issues \
         "queue":"TMP",
         "summary": "'"${RELEASE_TAG}"'",
         "type": "task",
-        "description": "Hello world! zhcoder",
+        "description": "AUTHOR: '${AUTHOR}'\n CHANGELOG: '${CHANGELOG}'",
         "unique": "https://github.com/mezhcoder/shri-infastructure/releases/tag/'${RELEASE_TAG}'"
     }'
 )
